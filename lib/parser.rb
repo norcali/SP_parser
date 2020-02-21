7a0eb4592254ecbdb028943d63296c8374facff7
Nicolas Orcali <orcalinicolas@gmail.com>
2020-02-21 03:23:06 -0300

    Tests setup done, Parser class setup done

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,4 +1,26 @@
 # frozen_string_literal: true
 
+PATH = File.expand_path('..', __dir__)
+require_relative 'reader'
+require 'json'
+
 class Parser
+  attr_reader :log_data, :file_path
+
+  def initialize(file_path)
+  end
+
+  def parse_for_humans
+  end
+
+  def run
+  end
+
+  private
+
+  def unique_visitors
+  end
+
+  def visitors
+  end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+PATH = File.expand_path('..', __dir__)
 require 'spec_helper'
 require 'reader'
 require 'parser'
@@ -10,15 +11,31 @@ RSpec.describe Parser do
     let(:parser) { Parser.new(log_file_path) }
 
     it 'should find /home as the most visited' do
+      expect(parser.send(:visitors).first.first).to eq('/home')
     end
 
     it '/home should have been visited 5 times' do
+      expect(parser.send(:visitors).first.last).to eq('5 visits')
     end
 
     it 'should find /home as the most uniquely visited' do
+      expect(parser.send(:unique_visitors).first.first).to eq('/home')
     end
 
     it '/home should have been uniquely visited 4 times' do
+      expect(parser.send(:unique_visitors).first.last).to eq('4 unique views')
+    end
+
+    before { parser.run }
+
+    it 'should create the json file' do
+      expect(File).to exist("#{PATH}/spec/fixtures/test.log.json")
+    end
+
+    before { parser.parse_for_humans }
+
+    it 'should create the json file' do
+      expect(File).to exist("#{PATH}/for_humans.json")
     end
 
   end
