c3e764cebc432dc521718423d7e0e9c05fd25149
Nicolas Orcali <orcalinicolas@gmail.com>
2020-02-21 02:29:09 -0300

    Added reader class file, an updated the spec for a fail state

--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -10,12 +10,21 @@ RSpec.describe Reader do
 
     before { reader.load }
 
-    it 'finds 6 urls' do
-      expect(reader.log_data.count).to eq 6
+    it 'finds 7 urls' do
+      expect(reader.log_data.count).to eq 7
     end
 
     it 'finds 17 visitors' do
       expect(reader.log_data.values.flatten.count).to eq 17
+    end
+  end
+
+  context 'when NOT finding file' do
+    let(:log_file_path) { 'spec/fixtures/wrong_test.log' }
+    let(:reader) { Reader.new(log_file_path) }
+
+    it 'raises an error' do
+      expect( reader.load ).to raise_error(Errno::ENOENT)
     end
   end
 end
